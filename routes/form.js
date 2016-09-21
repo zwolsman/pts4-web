67bd8c7a918423ff2df51118181750fc2e3479ee
Goos van den Bekerom <g.vandenbekerom@student.fontys.nl>
2016-09-21 17:57:12 +0200

    Added files for head, header and footer. Created send alert form (unfinished)

--- routes/form.js
+++ routes/form.js
@@ -2,22 +2,22 @@ var express = require('express');
 var router = express.Router();
 
 var NodeGeocoder = require('node-geocoder');
+var http = require("http");
 
 var options = {
     provider: 'google',
 
     // Optional depending on the providers
-    httpAdapter: 'https', // Default
-    apiKey: 'YOUR_API_KEY', // for Mapquest, OpenCage, Google Premier
-    formatter: null         // 'gpx', 'string', ...
+    httpAdapter: 'https',
+    apiKey: 'AIzaSyDJtdmnzbzoqydZt6sX-rQtsVob7JXERMc',
+    formatter: null
 };
 
 var geocoder = NodeGeocoder(options);
 
 var getLocation = function(searchQuery) {
-    geocoder.geocode(searchQuery)
+    geocoder.geocode('De Admirant, Eindhoven')
         .then(function(res) {
-            res.add
             console.log(res);
         })
         .catch(function(err) {
@@ -25,9 +25,32 @@ var getLocation = function(searchQuery) {
         });
 }
 
+function initMap() {
+    var mapDiv = document.getElementById('map');
+    var map = new google.maps.Map(mapDiv, {
+        center: {lat: 44.540, lng: -78.546},
+        zoom: 8
+    });
+};
+
+
+
 /* GET Form page. */
 router.get('/', function(req, res, next) {
-    res.render('form', { title: 'Meld een crisis' });
+
+    function requestMap(){
+        var maps_api = {
+            host: "maps.googleapis.com",
+            path: "/maps/api/js?key=AIzaSyDJtdmnzbzoqydZt6sX-rQtsVob7JXERMc"
+        }
+
+        http.get(maps_api, function(res){
+            console.log(res);
+            //initMap(?);
+        });
+    };
+
+    res.render('form', { title: 'Meld een crisis'/*, requestMap: requestMap()*/});
 });
 
 module.exports = router;
